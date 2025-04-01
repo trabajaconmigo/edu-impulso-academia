e110726625cb9ea6eff69a76ac2c5ebe7dddaab7
trabajaconmigo <imajstor2000@gmail.com>
2025-04-01 12:16:08 -0700

    dev2

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-import { supabase } from "@/lib/supabaseClient";
+
 
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
