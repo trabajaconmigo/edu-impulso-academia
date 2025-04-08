a8dd17da972b89f8260ab0a90105faa1b88831c8
trabajaconmigo <imajstor2000@gmail.com>
2025-04-08 15:56:01 -0700

    dev2

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+  extends: ["next", "next/core-web-vitals"],
+  rules: {
+    // preserve any existing rules you have
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
